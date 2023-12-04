84c84671617943bcfec0160de11e8f68ab25b81d
GouzzzGouzzz <gouz@DESKTOP-TU6S07P.localdomain>
2023-12-04 17:42:39 +0100

    ctrl+d and full enter working correctly

--- Server.cpp
+++ Server.cpp
@@ -17,13 +17,15 @@ void Server::get_msgs(int fd_client, char *buf) //ctrl+d 2 fois de suite casse t
 		this->_client_msgs.insert(std::pair<int,std::string>(fd_client,temp));
 	else
 		this->_client_msgs.insert(std::pair<int,std::string>(fd_client, this->_client_msgs.at(fd_client).append(temp)));
-	if (logClients(fd_client) == -1)
-		return ;
-	if (temp.find("\n",0) == temp.size())
+	if (temp.find("\n",0) == temp.size() - 1) //handle CTRL+D
 	{
 		std::cout << "FINAL MESSAGE IS :"<< this->_client_msgs.at(fd_client);
-		this->_client_msgs.erase(fd_client);
+		if (logClients(fd_client) == -1)
+			return ;
+		//this->_client_msgs.erase(fd_client);
 	}
+	else
+		return ;
 }
 
 int Server::logClients(int fd_client)
@@ -31,34 +33,34 @@ int Server::logClients(int fd_client)
 	client *ptr = this->_clients.at(fd_client); //probleme avec : "enter" en input et CTRL D
 	if (ptr->is_logged == false)
 	{
-		std::string msgs = this->_client_msgs.at(fd_client);
-		if (ptr->nick.empty() == true && msgs.empty() == false)
+		std::string *msgs = &(this->_client_msgs.at(fd_client));
+		if (ptr->nick.empty() == true && msgs->empty() == false)
 		{
-			if (msgs[0] == '\n' && msgs.size() == 1)
+			if ((*msgs)[0] == '\n' && msgs->size() == 1)
 			{
-				this->_client_msgs.at(fd_client).clear();
+				msgs->clear();
 				return -1;
 			}
-			ptr->nick = msgs.substr(0,msgs.find("\n")); //check for multi name
-			this->_client_msgs.at(fd_client).clear();
+			ptr->nick = msgs->substr(0,msgs->find("\n")); //check for multi name
+			msgs->clear();
 			send(fd_client, "Waiting for username :\n",24,0);
 		}
-		else if (ptr->user.empty() == true && msgs.empty() == false && ptr->nick.empty() == false)
+		else if (ptr->user.empty() == true && msgs->empty() == false && ptr->nick.empty() == false)
 		{
-			if (msgs[0] == '\n' && msgs.size() == 1)
+			if ((*msgs)[0] == '\n' && msgs->size() == 1)
 			{
-				this->_client_msgs.at(fd_client).clear();
+				msgs->clear();
+				std::cout << "CLEARED\n";
 				return -1;
 			}
-			ptr->user = msgs.substr(msgs.find("\n") + 1,msgs.size() - msgs.find("\n") - 2);
+			ptr->user = msgs->substr(0,msgs->find("\n"));
 			send(fd_client, "SUCCESFULLY LOGGED IN\n",23,0);
-			this->_client_msgs.at(fd_client).clear();
+			msgs->clear();
+			ptr->is_logged = true;
 		}
 		else
 			return -1;
-		if (ptr->user.empty() == false && ptr->nick.empty() == false)
-			ptr->is_logged = true;
-		else
+		if (ptr->is_logged == false)
 			return -1;
 	}
 	std::cout << "USER : " << ptr->user << " with nick : " << ptr->nick << " is logged : " << ptr->is_logged << std::endl;
